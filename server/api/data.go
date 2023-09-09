7e66c9be7f9c0e5faf7ed75bc09a7cef45595b55
czQery <czqery@gmail.com>
2023-09-09 13:51:53 +0200

    added dataset filling after parser

--- server/api/data.go
+++ server/api/data.go
@@ -17,16 +17,17 @@ type DataUser struct {
 }
 
 type DataUserSession struct {
-	Date   int64  `json:"date"`
-	Device string `json:"device"`
-	From   int    `json:"from"`
-	To     int    `json:"to"`
+	Date   int64  `json:"date,omitempty"`
+	Device string `json:"device,omitempty"`
+	From   int    `json:"from,omitempty"`
+	To     int    `json:"to,omitempty"`
 }
 
 func Data(c *fiber.Ctx) error {
 	var (
-		dir   = os.Args[1]
-		users []DataUser
+		dir            = os.Args[1]
+		users          []DataUser
+		sessionsPerDay = 1
 	)
 
 	files, err := os.ReadDir(dir)
@@ -34,6 +35,7 @@ func Data(c *fiber.Ctx) error {
 		tl.Log("readDir", err.Error(), "error")
 	}
 
+	// read all files in specified folder
 	for _, file := range files {
 		if !file.IsDir() {
 			tl.Log("API", "Data - reading log: "+file.Name(), "debug")
@@ -44,21 +46,26 @@ func Data(c *fiber.Ctx) error {
 			}
 
 			var (
-				search      bool
-				searchName  string
-				searchLogin []string
-				searchList  []DataUserSession
+				search               bool
+				searchName           string
+				searchLogin          []string
+				searchList           []DataUserSession
+				searchSessionsPerDay int
 			)
 
+			// read all lines in file
 			for _, fileLine := range strings.Split(string(fileData), "\r\n") {
 				fileP := strings.Split(fileLine, ";")
 
+				// mark session start
 				if fileP[0] == "login" && !search {
 					search = true
 					searchName = fileP[1]
 					searchLogin = fileP
+					searchSessionsPerDay = 1
 				}
 
+				// mark session end
 				if fileP[0] == "logoff" && search {
 					date, _ := time.Parse(timeLayout, searchLogin[3]+"-00:00")
 
@@ -67,12 +74,69 @@ func Data(c *fiber.Ctx) error {
 
 					tl.Log("API", "Data - session: "+fileP[1]+" from: "+strconv.Itoa(int(timeStart.Unix()/60))+" to: "+strconv.Itoa(int(timeEnd.Unix()/60)), "debug")
 
+					// count number of session in same day for this day
+					for i := len(searchList) - 1; i >= 0; i-- {
+						if searchList[i].Date == date.Unix() {
+							searchSessionsPerDay = searchSessionsPerDay + 1
+						}
+					}
+					if sessionsPerDay < searchSessionsPerDay {
+						sessionsPerDay = searchSessionsPerDay
+					}
+
 					searchList = append(searchList, DataUserSession{Date: date.Unix(), Device: fileP[2], From: int(timeStart.Unix() / 60), To: int(timeEnd.Unix() / 60)})
 					search = false
 				}
 			}
-
 			users = append(users, DataUser{Name: searchName, Sessions: searchList})
+		}
+	}
+
+	tl.Log("API", "Data - sessions per day: "+strconv.Itoa(sessionsPerDay), "debug")
+
+	// post parse adjustments
+	for u := 0; u < len(users); u++ {
+
+		var (
+			sDay      int64
+			sDayCount int
+		)
+
+		for si := 0; si < len(users[u].Sessions); si++ {
+			if sDay == 0 || sDayCount == sessionsPerDay {
+				sDay = users[u].Sessions[si].Date
+				sDayCount = 1
+
+				// fill missing end
+				if si == len(users[u].Sessions)-1 {
+					for i := 0; i < sessionsPerDay-1; i++ {
+						users[u].Sessions = append(users[u].Sessions, DataUserSession{})
+					}
+					break
+				}
+				continue
+			}
+
+			sDayCount = sDayCount + 1
+
+			if sDay != users[u].Sessions[si].Date {
+				// fill missing sessions in day
+				if sDayCount <= sessionsPerDay {
+					users[u].Sessions = append(users[u].Sessions, DataUserSession{})
+					copy(users[u].Sessions[si+1:], users[u].Sessions[si:])
+					users[u].Sessions[si] = DataUserSession{}
+				} else {
+					sDay = 0
+				}
+			}
+
+			// fill missing end
+			if si == len(users[u].Sessions)-1 && sDayCount != sessionsPerDay {
+				for i := 0; i < sessionsPerDay-sDayCount; i++ {
+					users[u].Sessions = append(users[u].Sessions, DataUserSession{})
+				}
+				break
+			}
 		}
 	}
 
